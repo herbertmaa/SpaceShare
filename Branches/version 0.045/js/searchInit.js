8d767b4f391853fe399d3b3132b9c31cac18aee3
yogiduzit <yverma2@my.bcit.ca>
2019-04-04 12:34:55 -0700

    Added version 0.045

--- Branches/version 0.045/js/searchInit.js
+++ Branches/version 0.045/js/searchInit.js
@@ -5,7 +5,7 @@ $('#submit').on('click', event => {
     address = address.replace(/,/g,"");
     var addressArray = address.split(" ");
     console.log(addressArray);
-    localStorage.setItem('City', addressArray[addressArray.length - 3]);
+    localStorage.setItem('City', addressArray[3]);
   localStorage.setItem('Address', address);
    if (!(($('#administrative_area_level_1').val() === "") && ($('#locality').val() === "")))  {
         document.location.href = "search.html";
