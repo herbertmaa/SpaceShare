b0baf0168e680152645aab0a049d8dca0e9bf2de
yogiduzit <yverma2@my.bcit.ca>
2019-04-05 13:59:13 -0700

    Upload Listing Images working full

--- Branches/version 0.06/js/search.js
+++ Branches/version 0.06/js/search.js
@@ -8,6 +8,21 @@ $(document).ready(function() {
    console.log(query)
 ;    /* Iterates through the ListingsTest object and then creates as many divs 
     as there are listings*/
+
+    var user = firebase.auth().currentUser;
+    var name, email, photoUrl, uid, emailVerified;
+
+    if (user != null) {
+        name = user.displayName;
+        email = user.email;
+        photoUrl = user.photoURL;
+        emailVerified = user.emailVerified;
+        uid = user.uid; // The user's ID, unique to the Firebase project. Do NOT use
+        // this value to authenticate with your backend server, if
+        // you have one. Use User.getToken() instead.
+    }
+
+
     query.on('value', function(snapshot) {
         snapshot.forEach(function (childSnapshot) {
             var cityInput = childSnapshot.val().City;
@@ -16,8 +31,9 @@ $(document).ready(function() {
             var lengthInput = childSnapshot.val().Length;
             var widthInput = childSnapshot.val().Width;
             var provinceInput = childSnapshot.val().Province;
-           $('<div class="card"><img class="card-img-top" src="" alt="Card image cap"><div class="card-body"><h5 class="card-title">' +   addressInput + '<br>' +   cityInput + '<br>'  + provinceInput + '</h5><p class="card-text">' + "Dimensions: " + lengthInput + ' x ' +  widthInput + ' x ' +  heightInput + '</p><a href="#" class="btn btn-primary">Request</a></div></div>').appendTo('#cards-container');
-           $('.card-img-top').attr('src', 'img/black.png');
+            var imgURL = childSnapshot.val().ListingImage;
+            console.log(imgURL);
+           $('<div class="card"><img class="card-img-top" src=' + imgURL + 'alt="Card image cap"><div class="card-body"><h5 class="card-title">' +   addressInput + '<br>' +   cityInput + '<br>'  + provinceInput + '</h5><p class="card-text">' + "Dimensions: " + lengthInput + ' x ' +  widthInput + ' x ' +  heightInput + '</p><a href="#" class="btn btn-primary">Request</a></div></div>').appendTo('#cards-container');
         });
     });
     $('#heightSort').on('click', function() {
@@ -35,9 +51,9 @@ $(document).ready(function() {
                     var lengthInput = childSnapshot.val().Length;
                     var widthInput = childSnapshot.val().Width;
                     var provinceInput = childSnapshot.val().Province;
-                    $('<div class="card"><img class="card-img-top" src="" alt="Card image cap"><div class="card-body"><h5 class="card-title">' +   addressInput + '<br>' +   cityInput + '<br>'  + provinceInput + '</h5><p class="card-text">' + "Dimensions: " + lengthInput + ' x ' +  widthInput + ' x ' +  heightInput + '</p><a href="#" class="btn btn-primary">Request</a></div></div>').appendTo('#cards-container');
-                    $('.card-img-top').attr('src', 'img/black.png');
-            });
+                    var imgURL = childSnapshot.val().ListingImage;
+                    $('<div class="card"><img class="card-img-top" src=' + imgURL + 'alt="Card image cap"><div class="card-body"><h5 class="card-title">' +   addressInput + '<br>' +   cityInput + '<br>'  + provinceInput + '</h5><p class="card-text">' + "Dimensions: " + lengthInput + ' x ' +  widthInput + ' x ' +  heightInput + '</p><a href="#" class="btn btn-primary">Request</a></div></div>').appendTo('#cards-container');
+                });
         }); 
     });
     $('#widthSort').on('click', function() {
@@ -55,18 +71,18 @@ $(document).ready(function() {
                     var lengthInput = childSnapshot.val().Length;
                     var widthInput = childSnapshot.val().Width;
                     var provinceInput = childSnapshot.val().Province;
-                    $('<div class="card"><img class="card-img-top" src="" alt="Card image cap"><div class="card-body"><h5 class="card-title">' +   addressInput + '<br>' +   cityInput + '<br>'  + provinceInput + '</h5><p class="card-text">' + "Dimensions: " + lengthInput + ' x ' +  widthInput + ' x ' +  heightInput + '</p><a href="#" class="btn btn-primary">Request</a></div></div>').appendTo('#cards-container');
-                    $('.card-img-top').attr('src', 'img/black.png');
-            });
+                    var imgURL = childSnapshot.val().ListingImage;
+                    $('<div class="card"><img class="card-img-top" src=' + imgURL + 'alt="Card image cap"><div class="card-body"><h5 class="card-title">' +   addressInput + '<br>' +   cityInput + '<br>'  + provinceInput + '</h5><p class="card-text">' + "Dimensions: " + lengthInput + ' x ' +  widthInput + ' x ' +  heightInput + '</p><a href="#" class="btn btn-primary">Request</a></div></div>').appendTo('#cards-container');
+                });
         }); 
     });
     $('#lengthSort').on('click', function() {
         var filterQuery = db.ref('/ListingsTest/').orderByChild('City_length').startAt(localStorage.getItem('City'));
         filterQuery.on('value', function(snapshot) {
-            console.log(snapshot);
+            
             $('#cards-container').empty();
             snapshot.forEach(function (childSnapshot) {
-               
+                console.log(snapshot.val());
 
                console.log(childSnapshot.val().City_height);
                     var cityInput = childSnapshot.val().City;
@@ -75,11 +91,10 @@ $(document).ready(function() {
                     var lengthInput = childSnapshot.val().Length;
                     var widthInput = childSnapshot.val().Width;
                     var provinceInput = childSnapshot.val().Province;
-                    $('<div class="card"><img class="card-img-top" src="" alt="Card image cap"><div class="card-body"><h5 class="card-title">' +   addressInput + '<br>' +   cityInput + '<br>'  + provinceInput + '</h5><p class="card-text">' + "Dimensions: " + lengthInput + ' x ' +  widthInput + ' x ' +  heightInput + '</p><a href="#" class="btn btn-primary">Request</a></div></div>').appendTo('#cards-container');
-                    $('.card-img-top').attr('src', 'img/black.png');
-            });
+                    var imgURL = childSnapshot.val().ListingImage;
+                    $('<div class="card"><img class="card-img-top" src=' + imgURL + 'alt="Card image cap"><div class="card-body"><h5 class="card-title">' +   addressInput + '<br>' +   cityInput + '<br>'  + provinceInput + '</h5><p class="card-text">' + "Dimensions: " + lengthInput + ' x ' +  widthInput + ' x ' +  heightInput + '</p><a href="#" class="btn btn-primary">Request</a></div></div>').appendTo('#cards-container');
+                });
         }); 
     });
-
 });
 
