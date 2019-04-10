052f2964a34988c1089d495814cd1c348fda07df
yogiduzit <yverma2@my.bcit.ca>
2019-04-09 19:24:16 -0700

    version 0.07 search added

--- Branches/version 0.07/js/search.js
+++ Branches/version 0.07/js/search.js
@@ -7,7 +7,6 @@ $(document).ready(function() {
 
     var user = firebase.auth().currentUser;
     var name, email, photoUrl, uid, emailVerified;
-    
 
     if (user != null) {
         name = user.displayName;
@@ -43,7 +42,7 @@ $(document).ready(function() {
     });
 
     function showListings(database) {
-        var cityInput, addressInput, heightInput, lengthInput, widthInput, provinceInput, imgURL, descrip;
+        var cityInput, addressInput, heightInput, lengthInput, widthInput, provinceInput, imgURL, descrip, key;
 
         database.on('value', function(snapshot) {
             snapshot.forEach(function (childSnapshot) {
@@ -56,9 +55,11 @@ $(document).ready(function() {
                 provinceInput = childSnapshot.val().Province;
                 imgURL = childSnapshot.val().ListingImage;
                 descrip = childSnapshot.val().Description; 
-
+                key = childSnapshot.val().key;
                 if (childSnapshot.val().RentedOut == "NULL") {
-                    createCard(cityInput, addressInput, heightInput, lengthInput, widthInput, provinceInput, imgURL, descrip);
+                    createCard(cityInput, addressInput, heightInput, lengthInput, widthInput, provinceInput, imgURL, descrip, key);
+                } else {
+                    createRentedCard(cityInput, addressInput, heightInput, lengthInput, widthInput, provinceInput, imgURL, descrip, key);
                 }
             });
         });
@@ -66,7 +67,7 @@ $(document).ready(function() {
     }
 
     // Creates a card with the given inputs.
-    function createCard(city, address, height, length, width, province, url, description) {
+    function createCard(city, address, height, length, width, province, url, description, key) {
         let card = $('<div class = "card"></div>');
         let cardImg = $('<img class = "card-img-top" alt= "Listing Image">');
         if (url != "NULL") {
@@ -78,7 +79,7 @@ $(document).ready(function() {
         let cardTitle = $('<h5 class = "card-title">' + address + '<br>' + city + '<br>' + province + '</h5>');
         let cardText = $('<p class = "card-text">Length: ' + length + "&nbspWidth: " + width + '&nbspHeight: ' +  height + '</p>');
         let cardDescrip = $('<p class = "card-text id = "description">' + description + '</p>');
-        let button = $('<button type = "button" class = "btn btn-primary request" data-target = "exampleModal" data-toggle = "modal">Request</button>');
+        let button = $('<button type = "button" class = "btn btn-primary request" data-target = "exampleModal" data-toggle = "modal" data-key =' + key + '>Request</button>');
 
         
         cardBody.append(cardTitle).append(cardText).append(cardDescrip).append(button);
@@ -88,20 +89,49 @@ $(document).ready(function() {
 
         
     }
+    function createRentedCard(city, address, height, length, width, province, url, description, key) {
+        let card = $('<div class = "card"></div>');
+        let cardImg = $('<img class = "card-img-top" alt= "Listing Image">');
+        if (url != "NULL") {
+        cardImg.attr('src', url);
+        } else {
+            cardImg.attr('src', "https://images.unsplash.com/photo-1467385829985-2b0fb82b5193?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80");
+        }
+        let cardBody = $('<div class = "card-body"></div>');
+        let cardTitle = $('<h5 class = "card-title">' + address + '<br>' + city + '<br>' + province + '</h5>');
+        let cardText = $('<p class = "card-text">Length: ' + length + "&nbspWidth: " + width + '&nbspHeight: ' +  height + '</p>');
+        let cardDescrip = $('<p class = "card-text id = "description">' + description + '</p>');
+        let button = $('<button type = "button" class = "btn btn-primary request" data-target = "exampleModal" data-toggle = "modal" data-key =' + key + ' disabled>Reserved</button>');
 
-});
-$('#cards-container').on('click', '.request', function() {
-    $('#exampleModal').modal('toggle');
-});
+        
+        cardBody.append(cardTitle).append(cardText).append(cardDescrip).append(button);
+        card.append(cardImg);
+        card.append(cardBody);
+        $('#cards-container').append(card);
 
-
-$('#requestListing').on('click', function() {
-    var regex = new RegExp("REQUEST");
-    if ($('#request').val().match(regex)) {
-        window.location.href = "request.html";
-    } else {
-        alert("Invalid Entry");
-        $('#request').trigger('reset');
+        
     }
+    $('#cards-container').on('click', '.request', function() {
+        var button = $(this);
+        var key = button.attr('data-key');
+        
+        $('#exampleModal').modal('toggle');
+        $('#requestListing').on('click', function() {
+            var regex = new RegExp("REQUEST");
+            if ($('#request').val().match(regex)) {
+                var request = db.ref('/Listings/' + key);
+                request.child('RentedOut').set('RENTED by' + uid);
+                window.location.href = 'request.html';
+            } else {
+                alert("Invalid Entry");
+                $('#request').trigger('reset');
+            }
+        });
+    });
+
 });
+
+
+
+
 
